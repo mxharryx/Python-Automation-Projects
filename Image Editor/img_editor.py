2f569dc6e3a2c41ef6271e02be238a53ac8daed3
Mohammed Harris <md.harry.98@gmail.com>
2023-08-17 13:22:00 -0400

    Image editor

---
+++ Image Editor/img_editor.py
@@ -0,0 +1,25 @@
+from PIL import Image, ImageFilter
+import os
+
+#Specify source and dest directory
+source_directory - './image'
+destination_directory = './edited-img'
+
+#Ensure the destination directory exists
+
+os.makedirs(destination_directory, exist_ok=True)
+
+#all image files in the src dir
+image_files = [f for f in os.listdir(source_directory) if f.lower().endswith((".png", ".jpg", ".jpeg"))]
+
+for image_file in image_files:
+    source_path = os.path.join(source_directory, image_file)
+    destination_path = os.path.join(destination_directory, image_file)
+
+    image = Image.open(source_path)
+    red_filtered_image = image.filter(ImageFilter.Colorize(128, 0, 0, 0))
+    red_filtered_image.save(destination_path)
+
+    print(f"Processed '{image_file}'")
+
+    print("Image editing and saving complete.")
